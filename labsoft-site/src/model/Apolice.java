8b0d7dcecda2350271d332d56616dbb614c15f90
Elton Okawa <elton_okawa@hotmail.com>
2018-10-25 17:38:56 -0200

    model Apolice

--- labsoft-site/src/model/Apolice.java
+++ labsoft-site/src/model/Apolice.java
@@ -3,6 +3,41 @@ package model;
 import java.util.Date;
 
 public class Apolice {
+	private int id;
 	private Date dataInicio;
 	private Date dataFim;
+	private String status;
+//	private Cliente cliente;
+//	private Veiculo veiculo;
+//	private Compra compra;
+	
+	public Apolice(){}
+	
+	public int getId(){
+		return id;
+	}
+	
+	public Date getDataInicio(){
+		return dataInicio;
+	}
+	
+	public Date getDataFim() {
+		return dataFim;
+	}
+	
+	public String getStatus() {
+		return status;
+	}
+	
+//	public Cliente getCliente() {
+//		return cliente;
+//	}
+//	
+//	public Veiculo getVeiculo() {
+//		return veiculo;
+//	}
+//	
+//	public Compra getCompra() {
+//		return compra;
+//	}
 }
