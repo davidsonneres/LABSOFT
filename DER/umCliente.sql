e46374f86187cc8daa2c5704e537aa156c18db06
Elton Okawa <elton_okawa@hotmail.com>
2018-11-08 15:52:03 -0200

    Modelo Cliente atualizado

---
+++ DER/umCliente.sql
@@ -0,0 +1,2 @@
+INSERT INTO Cliente (IdCliente, Nome, Endereco, CPF, Telefone, Email)
+VALUES (50, "Ernest S. Sims", "Avenida Fernando Nobre, 1017", "899.426.035-81", "(11) 7151-3377", "ErnestSSims@jourrapide.com");
