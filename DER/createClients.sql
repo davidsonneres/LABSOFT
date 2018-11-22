11c9c5abb8674844807eb8d710477907bad6c52f
Elton Okawa <elton_okawa@hotmail.com>
2018-11-22 14:47:43 -0200

    senha adicionada para cliente e corretor

--- DER/createClients.sql
+++ DER/createClients.sql
@@ -1,11 +1,11 @@
-INSERT INTO Cliente (IdCliente, Nome, Endereco, CPF, Telefone, Email, Sexo, Nacionalidade, DataNascimento, CNH)
-VALUES (10, "Bob Esponja", "Fundo do Mar", "111.111.111-12", "(11)99999-9999", "bob.esponja@gmail.com", "Masculino", "Americano", "1980-12-16", "35014701803");
+INSERT INTO Cliente (IdCliente, Nome, Endereco, CPF, Telefone, Email, Sexo, Nacionalidade, DataNascimento, CNH, Senha)
+VALUES (10, "Bob Esponja", "Fundo do Mar", "111.111.111-12", "(11)99999-9999", "bob.esponja@gmail.com", "Masculino", "Americano", "1980-12-16", "35014701803", "1234");
 
-INSERT INTO Cliente (IdCliente, Nome, Endereco, CPF, Telefone, Email, Sexo, Nacionalidade, DataNascimento, CNH)
-VALUES (20, "Bethany K. Braatz", "Rua Dublim, 1562", "119.404.584-71 ", "(27)6374-7949", "BethanyKBraatz@jourrapide.com", "Feminino", "Brasileira", "1991-06-17", "21137472610");
+INSERT INTO Cliente (IdCliente, Nome, Endereco, CPF, Telefone, Email, Sexo, Nacionalidade, DataNascimento, CNH, Senha)
+VALUES (20, "Bethany K. Braatz", "Rua Dublim, 1562", "119.404.584-71 ", "(27)6374-7949", "BethanyKBraatz@jourrapide.com", "Feminino", "Brasileira", "1991-06-17", "21137472610", "1234");
 
-INSERT INTO Cliente (IdCliente, Nome, Endereco, CPF, Telefone, Email, Sexo, Nacionalidade, DataNascimento, CNH)
-VALUES (30, "Carole E. Carson", "Rua Várzea do Paulista, 1336", "469.401.289-48", "(81)5465-8186", "CaroleECarson@jourrapide.com", "Feminino", "Húngara", "1979-10-12", "04348172474");
+INSERT INTO Cliente (IdCliente, Nome, Endereco, CPF, Telefone, Email, Sexo, Nacionalidade, DataNascimento, CNH, Senha)
+VALUES (30, "Carole E. Carson", "Rua Várzea do Paulista, 1336", "469.401.289-48", "(81)5465-8186", "CaroleECarson@jourrapide.com", "Feminino", "Húngara", "1979-10-12", "04348172474", "1234");
 
-INSERT INTO Cliente (IdCliente, Nome, Endereco, CPF, Telefone, Email, Sexo, Nacionalidade, DataNascimento, CNH)
-VALUES (40, "Lois J. Claussen", "Rua Nestor Alves de Campos, 744", "736.289.174-17", "(42)9807-7698", "LoisJClaussen@jourrapide.com", "Masculino", "Japonês", "1974-09-25", "63185863492");
+INSERT INTO Cliente (IdCliente, Nome, Endereco, CPF, Telefone, Email, Sexo, Nacionalidade, DataNascimento, CNH, Senha)
+VALUES (40, "Lois J. Claussen", "Rua Nestor Alves de Campos, 744", "736.289.174-17", "(42)9807-7698", "LoisJClaussen@jourrapide.com", "Masculino", "Japonês", "1974-09-25", "63185863492", "1234");
--- DER/umCliente.sql
+++ DER/umCliente.sql
@@ -1,2 +1,2 @@
-INSERT INTO Cliente (IdCliente, Nome, Endereco, CPF, Telefone, Email)
-VALUES (50, "Ernest S. Sims", "Avenida Fernando Nobre, 1017", "899.426.035-81", "(11) 7151-3377", "ErnestSSims@jourrapide.com");
+INSERT INTO Cliente (IdCliente, Nome, Endereco, CPF, Telefone, Email, Sexo, Nacionalidade, DataNascimento, CNH, Senha)
+VALUES (50, "Lauss J. Claussen", "Rua Campos, 754", "996.289.174-17", "(42)9807-1234", "Lololussen@jourrapide.com", "Masculino", "Japonês", "1974-12-25", "63187963492", "1234");
