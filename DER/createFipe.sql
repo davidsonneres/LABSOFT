a7fa5ad59139d1d730a69391332562d04965279a
Elton Okawa <elton_okawa@hotmail.com>
2018-11-27 15:35:53 -0200

    added script for veiculos and fipe

---
+++ DER/createFipe.sql
@@ -0,0 +1,11 @@
+INSERT INTO FIPE (IdVeiculo, ValorFipe)
+VALUES (1, 50000.00);
+
+INSERT INTO FIPE (IdVeiculo, ValorFipe)
+VALUES (2, 100000.00);
+
+INSERT INTO FIPE (IdVeiculo, ValorFipe)
+VALUES (3, 150000.00);
+
+INSERT INTO FIPE (IdVeiculo, ValorFipe)
+VALUES (4, 200000.00);
