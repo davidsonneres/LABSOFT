a6273c838837bf1b0311f0083aecb6ebcd67b2f3
Elton Okawa <elton_okawa@hotmail.com>
2018-11-29 17:57:02 -0200

    added some sql to populate db

---
+++ DER/createApolice.sql
@@ -0,0 +1,11 @@
+INSERT INTO Apolice (IdApolice, DataInicio, DataFim, Status)
+VALUES (1, "1990-05-03", "1995-10-10", "Cancelado");
+
+INSERT INTO Apolice (IdApolice, DataInicio, DataFim, Status)
+VALUES (2, "1990-05-03", "1995-10-10", "Ativo");
+
+INSERT INTO Apolice (IdApolice, DataInicio, DataFim, Status)
+VALUES (3, "1992-05-03", "1997-10-10", "Vencido");
+
+INSERT INTO Apolice (IdApolice, DataInicio, DataFim, Status)
+VALUES (20, "1996-05-03", "2000-10-10", "Pendente");
---
+++ DER/createFranquia.sql
@@ -0,0 +1,2 @@
+INSERT INTO Franquia (IdFranquia, TipoFranquia, ValorFranquia)
+VALUES (1, "Vidro", 5000.00);
