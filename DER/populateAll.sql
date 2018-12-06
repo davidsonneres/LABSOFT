5c725d400ce267570c26a42bf4554b21ac181dd0
Elton Okawa <elton_okawa@hotmail.com>
2018-12-06 15:07:49 -0200

    changed populateall

--- DER/populateAll.sql
+++ DER/populateAll.sql
@@ -28,7 +28,7 @@ INSERT INTO Apolice (IdApolice, DataInicio, DataFim, Status)
 VALUES (2, "1990-05-03", "1995-10-10", "Ativo");
 
 INSERT INTO Apolice (IdApolice, DataInicio, DataFim, Status)
-VALUES (3, "1992-05-03", "1997-10-10", "Vencido");
+VALUES (3, "1992-05-03", "1997-10-10", "Encerrado");
 
 INSERT INTO Apolice (IdApolice, DataInicio, DataFim, Status)
 VALUES (20, "1996-05-03", "2000-10-10", "Pendente");
